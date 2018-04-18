370eb043ce10005805f2770cb4e893ee6bfc0ade
Martin Gorupić <37804840+MARTIN111Zg@users.noreply.github.com>
2018-04-19 00:21:01 +0200

    Add files via upload

---
+++ js.971e4e3a.js
@@ -0,0 +1,6 @@
+parcelRequire=function(e,r,n){var t="function"==typeof parcelRequire&&parcelRequire,i="function"==typeof require&&require;function u(n,o){if(!r[n]){if(!e[n]){var f="function"==typeof parcelRequire&&parcelRequire;if(!o&&f)return f(n,!0);if(t)return t(n,!0);if(i&&"string"==typeof n)return i(n);var c=new Error("Cannot find module '"+n+"'");throw c.code="MODULE_NOT_FOUND",c}a.resolve=function(r){return e[n][1][r]||r};var l=r[n]=new u.Module(n);e[n][0].call(l.exports,a,l,l.exports)}return r[n].exports;function a(e){return u(a.resolve(e))}}u.isParcelRequire=!0,u.Module=function(e){this.id=e,this.bundle=u,this.exports={}},u.modules=e,u.cache=r,u.parent=t;for(var o=0;o<n.length;o++)u(n[o]);return u}({10:[function(require,module,exports) {
+
+},{}],5:[function(require,module,exports) {
+"use strict";function e(){document.getElementById("icon").onclick=function(){document.getElementById("icon");document.getElementById("MyElement").classList.toggle("menu-active");var e=document.getElementById("change").classList;e.contains("fa-bars")?e.remove("fa-bars"):(e.add("fa-bars"),document.getElementById("change").style.color="#4b21ff"),e.contains("fa-times")?e.remove("fa-times"):(e.add("fa-times"),document.getElementById("change").style.color="white")}}require("../scss/main.scss"),window.onload=e;
+},{"../scss/main.scss":10}]},{},[5])
+//# sourceMappingURL=/js.971e4e3a.map
